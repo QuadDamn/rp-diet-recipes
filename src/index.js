505fd522c96233cf82edff663f88aac6ea645d8b
Shane Jeffery <nobull.wow@gmail.com>
2020-03-01 23:41:18 -0800

    Fix base doman for auth0.

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ ReactDOM.render(
     <Auth0Provider
         domain={process.env.REACT_APP_AUTH0_DOMAIN}
         client_id={process.env.REACT_APP_AUTH0_CLIENT_ID}
-        redirect_uri="http://localhost:3000"
+        redirect_uri={process.env.REACT_APP_BASE_DOMAIN}
         onRedirectCallback={onRedirectCallback}
     >
         <Provider store={store}>
